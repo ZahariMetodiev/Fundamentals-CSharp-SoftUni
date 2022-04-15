cd8b0d6fbf7a8d4ec7da3f5c4a846c57ebe5b040
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-15 09:46:19 +0300

    Add task EvenNumber

---
+++ E01.Basic Syntax, Conditional Statements and Loops/T12EvenNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Т12EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(number)}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+
+                number = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
