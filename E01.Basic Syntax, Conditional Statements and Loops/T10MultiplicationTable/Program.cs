a60625c5c8a502fa7f9a955bf25a8a52a07552e9
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-15 09:45:31 +0300

    Add task MultiplicationTable

---
+++ E01.Basic Syntax, Conditional Statements and Loops/T10MultiplicationTable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T10MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numbers} X {i} = {numbers * i}");
+            }
+        }
+    }
+}
