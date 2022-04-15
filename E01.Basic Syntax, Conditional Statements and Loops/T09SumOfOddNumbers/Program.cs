3d114c87de4fb77074a15470cc5f093f2f44f0d2
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-15 09:45:06 +0300

    Add task SumOfOddNumbers

---
+++ E01.Basic Syntax, Conditional Statements and Loops/T09SumOfOddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T09SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= numbers * 2; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
