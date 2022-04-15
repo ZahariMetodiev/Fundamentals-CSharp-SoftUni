c8d408e196f211433d2b5009bc01f25e70655a98
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-15 09:40:39 +0300

    Add task BackIn30Minutes

---
+++ E01.Basic Syntax, Conditional Statements and Loops/T04BackIn30Minutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T04BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minute = int.Parse(Console.ReadLine()) + 30;
+
+            if (minute >= 60)
+            {
+                hour++;
+                minute = minute % 60;
+            }
+
+            if (hour > 23)
+            {
+                hour = 0;
+            }
+
+            Console.WriteLine($"{hour}:{minute:d2}");
+        }
+    }
+}
