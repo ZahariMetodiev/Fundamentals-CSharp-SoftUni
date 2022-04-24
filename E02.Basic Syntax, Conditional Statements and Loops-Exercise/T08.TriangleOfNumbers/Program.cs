c1d15cf3122f0f86cbbcccb64c61d6f37518a6e5
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-24 09:25:01 +0300

    Add task TriangleOfNumbers

---
+++ E02.Basic Syntax, Conditional Statements and Loops-Exercise/T08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T08TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int  line= 1; line <= num; line++)
+            {
+                for (int rows = 1; rows <= line; rows++)
+                {
+                    Console.Write($"{line} ");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
