d8fbb88c899284579835ae23f29a0fbf67de96d3
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-24 09:23:31 +0300

    Add task PrintAndSum

---
+++ E02.Basic Syntax, Conditional Statements and Loops-Exercise/T04.PrintAndSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T04PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fyrstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = fyrstNumber; i <= secondNumber; i++)
+            {
+                sum += i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.Write($"Sum: {sum}");
+        }
+    }
+}
