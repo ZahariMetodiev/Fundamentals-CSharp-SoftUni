a0fd35098c6bf4bcb8aba82173f3ff9533fac0e6
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-24 09:25:49 +0300

    Add task RageExpenses

---
+++ E02.Basic Syntax, Conditional Statements and Loops-Exercise/T10.RageExpenses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T10RageExpenses
+{
+    class Program
+    {
+        int lostGames = int.Parse(Console.ReadLine());
+        double headsetPrice = double.Parse(Console.ReadLine());
+        double mousePrice = double.Parse(Console.ReadLine());
+        double keyboardPrice = double.Parse(Console.ReadLine());
+        double displayPrice = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i<lo; i++)
+			{
+
+			}
+    }
+}
