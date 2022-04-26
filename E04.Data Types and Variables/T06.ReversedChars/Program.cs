38c3ea41512d8fbc2d8eca9dc1bcaa689f5762ee
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-26 09:42:55 +0300

    Add task ReversedChars

---
+++ E04.Data Types and Variables/T06.ReversedChars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T06.ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+
+            Console.Write($"{thirdChar} {secondChar} {firstChar}");
+
+        }
+    }
+}
