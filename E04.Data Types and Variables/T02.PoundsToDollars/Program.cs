a34d5f6443ef63313101e8562cfdcf7b699f6857
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-26 09:40:43 +0300

    Add task PoundsToDollars

---
+++ E04.Data Types and Variables/T02.PoundsToDollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T02.PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal GBP = decimal.Parse(Console.ReadLine());
+
+            decimal USD = GBP * 1.31m;
+
+            Console.WriteLine($"{USD:f3}");
+        }
+    }
+}
