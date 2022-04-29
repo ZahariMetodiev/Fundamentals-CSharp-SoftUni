a542d554846a19b6a5c644f8ee69f308c60332de
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:24:06 +0300

    Add task CalculateRectangleArea

---
+++ E07.Methods/T06CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T06CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double widht = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double result = CalculateRectangleArea(widht, height);
+            Console.WriteLine(result);
+        }
+
+        static double CalculateRectangleArea(double widht , double height)
+        {
+            return widht * height;
+        }
+    }
+}
