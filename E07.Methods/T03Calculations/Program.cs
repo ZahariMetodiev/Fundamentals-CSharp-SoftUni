b02ff63f9fa4554f040201ee6bec313091f52953
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:22:44 +0300

    Add task Calculations

---
+++ E07.Methods/T03Calculations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Calculations(command, firstNum, secondNum);
+        }
+
+        static void Calculations(string command, int first, int second)
+        {
+            switch (command)
+            {
+                case "add":
+                    Console.WriteLine(first + second);
+                    break;
+                case "subtract":
+                    Console.WriteLine(first - second);
+                    break;
+                case "multiply":
+                    Console.WriteLine(first * second);
+                    break;
+                case "divide":
+                    Console.WriteLine(first / second);
+                    break;
+            }
+        }
+    }
+}
