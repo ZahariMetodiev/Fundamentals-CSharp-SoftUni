55f29c689f893c036feec230ce4ef4991beb69f8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:21:55 +0300

    Add task SignOfIntegerNumbers

---
+++ E07.Methods/T01SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T01SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SignOfIntegerNumbers(num));
+        }
+
+        static string SignOfIntegerNumbers(int number)
+        {
+            string sign = "";
+            
+            if (number > 0)
+            {
+                sign = "positive";
+            }
+            else if (number < 0)
+            {
+                sign = "negative";
+            }
+            else if (number == 0)
+            {
+                sign = "zero";
+            }
+
+            return $"The number {number} is {sign}.";
+        }
+    }
+}
