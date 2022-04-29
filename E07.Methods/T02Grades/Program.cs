78d40eec3311a0fea005a4d88633437c5d0fba2c
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:22:22 +0300

    Add task Grades

---
+++ E07.Methods/T02Grades/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T02Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            
+            string result = Grades(grade);
+
+            Console.WriteLine(result);
+        }
+
+        static string Grades(double grade)
+        {
+            
+            string sign = string.Empty;
+
+            if (grade >= 2 && grade <= 2.99)
+            {
+                sign = "Fail";
+            }
+            else if (grade <= 3.49)
+            {
+                sign = "Poor";
+            }
+            else if (grade <= 4.49)
+            {
+                sign = "Good";
+            }
+            else if (grade <= 5.49)
+            {
+                sign = "Very good";
+            }
+            else if (grade <= 6)
+            {
+                sign = "Excellent";
+            }
+
+            return sign;
+        }
+    }
+}
