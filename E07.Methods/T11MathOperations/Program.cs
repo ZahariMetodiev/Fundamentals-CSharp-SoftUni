2ebf186eaad6e547ba10349e252623de0299ddfa
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:26:17 +0300

    Add task MathOperations

---
+++ E07.Methods/T11MathOperations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace T11MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fyrstNumber = int.Parse(Console.ReadLine());
+            string operatoR = Console.ReadLine();
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            double result = Calculate(fyrstNumber, operatoR, secondNumber);
+            Console.WriteLine(result);
+        }
+
+        static double Calculate(int a, string operatoR, int b)
+        {
+            double result = 0;
+
+            if (operatoR == "*")
+            {
+                result = a * b;
+            }
+            else if (operatoR == "+")
+            {
+                result = a + b;
+            }
+            else if (operatoR == "-")
+            {
+                result = a - b;
+            }
+            else if (operatoR == "/")
+            {
+                result = a / b;
+            }
+
+            return result;
+        }
+    }
+}
