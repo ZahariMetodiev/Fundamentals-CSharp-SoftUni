cbbb585f5c60688ad2145575d0342f307a45d81c
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:23:12 +0300

    Add task PrintingTriangle

---
+++ E07.Methods/T04PrintingTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T04.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                PrintNumbers(i);
+            }
+            for (int i = n - 1; i > 0; i--)
+            {
+                PrintNumbers(i);
+            }
+        }
+
+        static void PrintNumbers(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
