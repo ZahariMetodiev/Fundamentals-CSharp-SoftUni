c03843e5ea36d8af3fee3b37d733b8cf2a63330a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:24:30 +0300

    Add task RepeatString

---
+++ E07.Methods/T07RepeatString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T07RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(input, count);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatString(string text, int count)
+        {
+            string result = "";
+
+            for (int i = 0; i < count; i++)
+            {
+                result += text;
+            }
+            return result;
+        }
+    }
+}
