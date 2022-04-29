69a8b4d8f5fba8027f47603f8572e1cc737e02b8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:25:19 +0300

    Add task GreaterOfTwoValues

---
+++ E07.Methods/T09GreaterOfTwoValues/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace T09GreaterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int result = GetMaxIntValue();
+                Console.WriteLine(result);
+            }
+            else if (type == "char")
+            {
+                char result = GetMaxCharValue();
+                Console.WriteLine(result);
+            }
+            else if (type == "string")
+            {
+                string result = GetMaxStringValue();
+                Console.WriteLine(result);
+            }
+        }
+
+        static int GetMaxIntValue()
+        {
+            int fyrstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            int maxNum = Math.Max(fyrstNum, secondNum);
+            return maxNum;
+        }
+
+        static char GetMaxCharValue()
+        {
+            char fyrstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            int maxChar = Math.Max(fyrstChar, secondChar);
+            return Convert.ToChar(maxChar);
+        }
+
+        static string GetMaxStringValue()
+        {
+            string fyrst = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            int fyrstSum = 0;
+            int secondSum = 0;
+
+            for (int i = 0; i < fyrst.Length; i++)
+            {
+                fyrstSum += fyrst[i];
+            }
+
+            for (int i = 0; i < second.Length; i++)
+            {
+                secondSum += second[i];
+            }
+
+            if (fyrstSum > secondSum)
+            {
+                return fyrst;
+            }
+            return second;
+        }
+    }
+}
