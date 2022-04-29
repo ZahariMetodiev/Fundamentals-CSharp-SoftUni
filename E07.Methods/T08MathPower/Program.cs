81401815a8ff6cdd301cb686ab4407e6467da7fe
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-29 08:24:50 +0300

    Add task MathPower

---
+++ E07.Methods/T08MathPower/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T08MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double result = MathPower(number, power);
+            Console.WriteLine(result);
+        }
+
+        static double MathPower(double number, double power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
