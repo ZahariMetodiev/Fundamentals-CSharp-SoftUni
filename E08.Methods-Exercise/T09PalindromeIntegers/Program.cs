b5fdb5a5804408cd97d82697d5d5fe8d79548c18
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-30 08:51:02 +0300

    Add task PalindromeIntegers

---
+++ E08.Methods-Exercise/T09PalindromeIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace T09PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                Console.WriteLine(IsPalindrome(input).ToString().ToLower());
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool IsPalindrome(string text)
+        {
+            char[] reversed = text.Reverse().ToArray();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!(reversed[i] == text[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+            
+        }
+    }
+}
