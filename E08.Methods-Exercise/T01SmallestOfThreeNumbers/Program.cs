03501e688110cc2f650f829d08fd16ad3e4758c8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-30 08:46:51 +0300

    Add task SmallestOfThreeNumbers

---
+++ E08.Methods-Exercise/T01SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T01SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fyrstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = FindSmallestOfThreeNumbers(fyrstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        static int FindSmallestOfThreeNumbers(int a, int b, int c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+    }
+}
