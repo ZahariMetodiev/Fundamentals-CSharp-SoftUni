290899b92bfc28522062193cab54c40ac7762181
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-30 08:50:02 +0300

    Add task NxNMatrix

---
+++ E08.Methods-Exercise/T07NxNMatrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T07NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNumbers(n);
+        }
+
+        static void PrintNumbers(int number)
+        {
+            for (int row = 0; row < number; row++)
+            {
+                for (int col = 0; col < number; col++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
