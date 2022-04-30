dbe5ee28c1e7de7a3e4929d54466aff7877ead16
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-30 08:50:40 +0300

    Add task FactorialDivision

---
+++ E08.Methods-Exercise/T08FactorialDivision/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T08FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            decimal result = CalculateFactorial(num1) / CalculateFactorial(num2);
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static decimal CalculateFactorial(int number)
+        {
+            decimal factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
