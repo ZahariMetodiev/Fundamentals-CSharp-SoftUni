c057ffb75acc12c9b2fd2f231a8116717d7f9353
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-30 08:47:24 +0300

    Add task VowelsCount

---
+++ E08.Methods-Exercise/T02VowelsCount/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T02VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+
+            int result = VowelsCount(input);
+            Console.WriteLine(result);
+        }
+
+        static int VowelsCount(string text)
+        {
+            int vowelsCount = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+                
+                if (currentSymbol == 'a' || currentSymbol == 'e' || currentSymbol == 'i'
+                    || currentSymbol == 'o' || currentSymbol == 'u' || currentSymbol == 'y')
+                {
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+    }
+}
