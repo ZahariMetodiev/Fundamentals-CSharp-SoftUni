017c92ab6ddc0094576d81167726738322cdc79e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-04-30 08:49:30 +0300

    Add task MiddleCharacters

---
+++ E08.Methods-Exercise/T06MiddleCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T06MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string result = GetMiddleCharacters(input);
+            Console.WriteLine(result);
+        }
+
+        static string GetMiddleCharacters(string text)
+        {
+            string result = "";
+
+            if (text.Length % 2 == 0)
+            {
+                result = text[text.Length / 2 - 1].ToString() + text[text.Length / 2].ToString();
+            }
+            else
+            {
+                result = text[text.Length / 2].ToString();
+            }
+
+            return result;
+        }
+    }
+}
