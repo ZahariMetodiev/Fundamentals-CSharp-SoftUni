de2af3a5d6305ad1e4eb94d967eab7ec5abad319
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-02 00:26:05 +0300

    Add task PrintNumbersInReverseOrder

---
+++ E10.Arrays/T02PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace T02PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(' ',numbers.Reverse()));
+        }
+    }
+}
