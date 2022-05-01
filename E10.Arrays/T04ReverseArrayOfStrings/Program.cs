d2ae9be60204c7ab53cc73be0e7996e81d8fd5bf
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-02 00:30:42 +0300

    Add task ReverseArrayOfStrings

---
+++ E10.Arrays/T04ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace T04ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(string.Join(' ', input.Reverse()));
+        }
+    }
+}
