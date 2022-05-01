6df44b30406367fc34e07d564f3e53a31012f7e6
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-02 00:30:10 +0300

    Add task RoundingNumbers

---
+++ E10.Arrays/T03RoundingNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace T03RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            int[] roundedNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < roundedNumbers.Length; i++)
+            {
+                roundedNumbers[i] = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+            }
+
+            for (int i = 0; i < roundedNumbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {roundedNumbers[i]}");
+            }
+        }
+    }
+}
