fb0fd3d7f8103342eafb5811e065590caeee8ad3
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-02 00:52:21 +0300

    Add task CondenseArrayToNumber

---
+++ E10.Arrays/T08CondenseArrayToNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace T08CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                sum += numbers[i] + numbers[i + 1];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
