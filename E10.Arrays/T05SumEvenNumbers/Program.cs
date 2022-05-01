c86163ce1356d21ff57fc57f614cbd565d7fb6c4
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-02 00:31:03 +0300

    Add task SumEvenNumbers

---
+++ E10.Arrays/T05SumEvenNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace T05SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
