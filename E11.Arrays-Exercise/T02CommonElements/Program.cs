7637bf0e98234e3fad2f996a0263645da0eb0586
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-03 09:05:05 +0300

    Add task CommonElements

---
+++ E11.Arrays-Exercise/T02CommonElements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T02CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] array2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                for (int j = 0; j < array1.Length; j++)
+                {
+                    if (array2[i] == array1[j])
+                    {
+                        Console.Write(array1[j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
