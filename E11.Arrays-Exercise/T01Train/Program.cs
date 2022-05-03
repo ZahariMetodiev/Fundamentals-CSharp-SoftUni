fbaf87e47b4b910b6c9b73d030fd7a56ab0e52d8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-03 09:04:42 +0300

    Add task Train

---
+++ E11.Arrays-Exercise/T01Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T01Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] wagons = new int[n];
+
+            int sum = 0;
+
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                wagons[i] = int.Parse(Console.ReadLine());
+                sum += wagons[i];
+            }
+
+            Console.WriteLine(string.Join(' ' , wagons));
+            Console.WriteLine(sum);
+        }
+    }
+}
