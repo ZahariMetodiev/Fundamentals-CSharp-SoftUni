eb7a8f2bd872a948aef58acc75d7849ed364b8a6
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-04 08:05:40 +0300

    Add task EncryptSort

---
+++ T12.Arrays-More Exercise/T01.EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace T01.EncryptSortAndPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] output = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int sum = 0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    char currentSymbol = input[j];
+
+                    if (currentSymbol == 'a' || currentSymbol == 'A'||
+                        currentSymbol == 'o' || currentSymbol == 'O'||
+                        currentSymbol == 'e' || currentSymbol == 'E'||
+                        currentSymbol == 'i' || currentSymbol == 'I'||
+                        currentSymbol == 'u' || currentSymbol == 'U')
+                    {
+                        sum += currentSymbol * input.Length;
+                    }
+                    else
+                    {
+                        sum += currentSymbol / input.Length;
+                    }
+                }
+                    output[i] = sum;
+            }
+            Array.Sort(output);
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
