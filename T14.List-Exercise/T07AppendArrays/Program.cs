ca218a26fb07c079892ebfeb13e46b43fc77c41f
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-06 08:44:39 +0300

    Add task AppendArrays

---
+++ T14.List-Exercise/T07AppendArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).Reverse().ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<string> currentList = numbers[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int j = 0; j < currentList.Count; j++)
+                {
+                    Console.Write($"{currentList[j]} ");
+                }
+
+            }
+        }
+    }
+}
