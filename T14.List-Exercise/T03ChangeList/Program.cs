3b3b87993f495affdb92828e6ac29c41cc93e91a
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-06 08:42:53 +0300

    Add task ChangeList

---
+++ T14.List-Exercise/T03ChangeList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] command = input.Split();
+
+                if (command[0] == "Delete")
+                {
+                    int number = int.Parse(command[1]);
+
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] == number)
+                        {
+                            numbers.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+                else
+                {
+                    int number = int.Parse(command[1]);
+                    int index = int.Parse(command[2]);
+
+                    numbers.Insert(index, number);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
