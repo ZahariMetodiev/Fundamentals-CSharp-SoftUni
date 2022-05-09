48ca0300427591499e844a8b5e4b6207d0d8de26
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-09 10:10:06 +0300

    Add task WordSynonyms

---
+++ T16.Associative Arrays/T03WordSynonyms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T03WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonyms = Console.ReadLine();
+
+                if (!(words.ContainsKey(word)))
+                {
+                    words.Add(word, new List<string>());
+                    words[word].Add(synonyms);
+                }
+                else
+                {
+                    words[word].Add(synonyms);
+                }
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
