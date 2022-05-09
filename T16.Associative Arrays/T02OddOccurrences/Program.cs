ec5c904389a0b7182f937537b621c39008c366f0
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-09 10:09:28 +0300

    Add task OddOccurrences

---
+++ T16.Associative Arrays/T02OddOccurrences/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string currentWord = word.ToLower();
+
+                if (counts.ContainsKey(currentWord))
+                {
+                    counts[currentWord]++;
+                }
+                else
+                {
+                    counts.Add(currentWord, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
