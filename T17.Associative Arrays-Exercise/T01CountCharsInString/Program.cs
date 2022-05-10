15c776e3971cf33c6b445e4c3e699839f5bb2e40
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-10 10:04:42 +0300

    Add task CountCharsInString

---
+++ T17.Associative Arrays-Exercise/T01CountCharsInString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (var word in words)
+            {
+                foreach (var symbol in word)
+                {
+                    if (counts.ContainsKey(symbol))
+                    {
+                        counts[symbol]++;
+                    }
+                    else
+                    {
+                        counts.Add(symbol, 1);
+                    }
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                Console.WriteLine($"{count.Key} -> {count.Value}");
+            }
+        }
+    }
+}
