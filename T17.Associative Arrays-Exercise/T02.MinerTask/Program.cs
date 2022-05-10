8c4acdfdbc8f459044e103eed5923a00ef824734
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-10 10:05:02 +0300

    Add task MinerTask

---
+++ T17.Associative Arrays-Exercise/T02.MinerTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T02.MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resourseInformation = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resourseInformation.ContainsKey(input))
+                {
+                    resourseInformation[input] += quantity;
+                }
+                else
+                {
+                    resourseInformation.Add(input, quantity);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var resourse in resourseInformation)
+            {
+                Console.WriteLine($"{resourse.Key} -> {resourse.Value}");
+            }
+        }
+    }
+}
