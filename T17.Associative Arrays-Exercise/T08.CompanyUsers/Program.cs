062f91830c63c84de5e1dd5103293734abe46b06
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-10 10:07:16 +0300

    Add task CompanyUsers

---
+++ T17.Associative Arrays-Exercise/T08.CompanyUsers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> output = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] companyInformations = input.Split(" -> ");
+
+                string companyName = companyInformations[0];
+                string employeeId = companyInformations[1];
+
+                if (output.ContainsKey(companyName))
+                {
+                    if (!output[companyName].Contains(employeeId))
+                    {
+                        output[companyName].Add(employeeId);
+                    }
+                }
+                else
+                {
+                    output.Add(companyName, new List<string>() { employeeId });
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in output)
+            {
+                Console.WriteLine(company.Key);
+
+                foreach (var item in company.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
