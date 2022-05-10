3bf3a5861e0274459c670ce1ac2f2b772a4db48d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-10 10:06:55 +0300

    Add task StudentAcademy

---
+++ T17.Associative Arrays-Exercise/T07. StudentAcademy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07._StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double>());
+                }
+
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                double averageGrade = student.Value.Average();
+
+                if (averageGrade >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {averageGrade:f2}");
+                }
+            }
+        }
+    }
+}
