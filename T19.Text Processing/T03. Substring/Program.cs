b5cade649609360191fd6e468a042a53f5f07726
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-12 17:17:03 +0300

    Add task Substring

---
+++ T19.Text Processing/T03. Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bannedWord = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int startIndex = text.IndexOf(bannedWord);
+
+            while (startIndex != -1)
+            {
+                text = text.Remove(startIndex, bannedWord.Length);
+                startIndex = text.IndexOf(bannedWord);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
