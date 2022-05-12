e4e1b3d6649cbef2c42ab87ce2d78a47fa7e2f70
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-12 17:17:56 +0300

    Add task DigitsLettersAndOther

---
+++ T19.Text Processing/T05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.DigitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string digit = string.Empty;
+            string letter = string.Empty;
+            string other = string.Empty;
+
+            foreach (var symbol in text)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    digit += symbol;
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    letter += symbol;
+                }
+                else
+                {
+                    other += symbol;
+                }
+            }
+
+            Console.WriteLine(digit);
+            Console.WriteLine(letter);
+            Console.WriteLine(other);
+        }
+    }
+}
