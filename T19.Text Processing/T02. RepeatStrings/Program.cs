a1d2fa75a9274783bc28e397750b134ce7aa150e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-12 17:16:42 +0300

    Add task RepeatStrings

---
+++ T19.Text Processing/T02. RepeatStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace T02._RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
