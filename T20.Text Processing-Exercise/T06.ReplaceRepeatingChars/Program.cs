2a6c12e3bc13064a1fa73a16ff1d6748aa14c6ec
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:17:34 +0300

    Add task ReplaceRepeatingChar

---
+++ T20.Text Processing-Exercise/T06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace T06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            char prevLetter = '\0';
+
+            foreach (var letter in text)
+            {
+                if (letter != prevLetter)
+                {
+                    result.Append(letter);
+                }
+                prevLetter = letter;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
