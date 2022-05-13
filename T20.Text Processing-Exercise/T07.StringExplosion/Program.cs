8aedb37d794bf02faca9447129325a3503a811e4
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:17:56 +0300

    Add task StringExplosion

---
+++ T20.Text Processing-Exercise/T07.StringExplosion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace T07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            int bomb = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+
+                if (currentSymbol == '>')
+                {
+                    bomb += text[i + 1] - '0';
+                    result.Append(currentSymbol);
+                }
+                else if (bomb > 0)
+                {
+                    bomb--;
+                }
+                else
+                {
+                    result.Append(currentSymbol);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
