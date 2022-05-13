a5d365cd6cb5e1c34433754536feed78fb77169d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:15:17 +0300

    Add task ValidUsername

---
+++ T20.Text Processing-Exercise/T01.ValidUsernames/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace T01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (var username in usernames)
+            {
+
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    bool isValid = true;
+
+                    foreach (var symbol in username)
+                    {
+                        if (!(char.IsLetterOrDigit(symbol) || symbol == '-' || symbol == '_'))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        Console.WriteLine(username);
+                    }
+                }
+            }
+        }
+
+    }
+}
