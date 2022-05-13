39038281add4c0d45dfcccfb0c43ce331a140664
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:16:21 +0300

    Add task ExtractFile

---
+++ T20.Text Processing-Exercise/T03.ExtractFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new char[] {'\\', '.'}, StringSplitOptions.RemoveEmptyEntries);
+
+            string fileName = input[input.Length -2];
+            string extension = input[input.Length-1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
