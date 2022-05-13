6c67b4804d27b87a42fd57b0c4f33cc4aaa09c2e
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:16:43 +0300

    Add task CaesarCipher

---
+++ T20.Text Processing-Exercise/T04.CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace T04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var symbol in text)
+            {
+                int replaceSymbol = symbol + 3;
+                stringBuilder.Append(Convert.ToChar(replaceSymbol));
+            }
+
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
