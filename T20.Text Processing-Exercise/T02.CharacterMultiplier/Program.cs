bfe3f45d5bfc624d34de12617aa0cefc87335d7b
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:16:01 +0300

    Add task CharacterMultiplier

---
+++ T20.Text Processing-Exercise/T02.CharacterMultiplier/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace T02.CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+            int sum = 0;
+            string firstWord = words[0];
+            string secondWord = words[1];
+
+            int minLenght = Math.Min(firstWord.Length, secondWord.Length);
+
+            for (int i = 0; i < minLenght; i++)
+            {
+                sum += firstWord[i] * secondWord[i];
+            }
+
+            if (firstWord.Length > secondWord.Length)
+            {
+                for (int i = minLenght; i < firstWord.Length; i++)
+                {
+                    sum += firstWord[i];
+                }
+            }
+            else
+            {
+                for (int i = minLenght; i < secondWord.Length; i++)
+                {
+                    sum += secondWord[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
