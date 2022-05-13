032f093496ce280e334b30cf23fb8fefe40321a7
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-13 10:17:08 +0300

    Add task MultiplyBigNumber

---
+++ T20.Text Processing-Exercise/T05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Decimal firstNumber = ulong.Parse(Console.ReadLine());
+            Decimal secondNumber = ulong.Parse(Console.ReadLine());
+
+            Decimal result = firstNumber * secondNumber;
+
+            Console.WriteLine(result);
+        }
+    }
+}
