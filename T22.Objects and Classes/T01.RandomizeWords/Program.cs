3a6cb11fe19a193b683bc3e73980defce5a0fa54
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-15 11:37:47 +0300

    Add task RandomizeWords

---
+++ T22.Objects and Classes/T01.RandomizeWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split();
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int index = random.Next(words.Length);
+                (words[i], words[index]) = (words[index], words[i]);
+            }
+
+            Console.WriteLine(string.Join("\n", words));
+        }
+    }
+}
