ab74befc51a126f0292f53a1140d24334c3d1e60
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-16 11:14:23 +0300

    Add task Articles2.0

---
+++ T23.Objects and Classes-Exercise/T03.Articles2.0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T03.Articles2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfArticles = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string[] data = Console.ReadLine().Split(", ");
+
+                Article newArticle = new Article(data[0], data[1], data[2]);
+                articles.Add(newArticle);
+            }
+
+            foreach (Article article in articles)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
