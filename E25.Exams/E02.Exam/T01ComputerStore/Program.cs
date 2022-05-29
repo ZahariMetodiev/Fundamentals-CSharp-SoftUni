901bcccd4daab0610a862b7ebe28c3dd6f1fe53d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-05-29 13:41:16 +0300

    Add task ComputerStore

---
+++ E25.Exams/E02.Exam/T01ComputerStore/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace T01ComputerStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double tax = 0;
+            
+            string input = Console.ReadLine();
+            
+            while (input != "special" && input != "regular")
+            {
+                double price = double.Parse(input);
+
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    sum += price;
+                    tax += price * 0.20;
+                }
+                
+                input = Console.ReadLine();
+            }
+
+            if (sum == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+
+            double totalSum = sum + tax;
+
+            if (input == "special")
+            {
+                totalSum *= 0.90;
+            }
+
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {sum:f2}$");
+            Console.WriteLine($"Taxes: {tax:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalSum:f2}$");
+        }
+    }
+}
